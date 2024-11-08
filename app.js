759730fd857450c3c1b830ad98973e102b349dda
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-08 07:47:40 +0100

    feat: add .gitignore

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,8 @@
 // controllers/authorController.js
+const asyncHandler = require("express-async-handler");
 
 const db = require("../db");
-
-const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 // The function will automatically catch any errors thrown and call the next function
 const getAuthorById = asyncHandler(async (req, res) => {
@@ -11,8 +11,7 @@ const getAuthorById = asyncHandler(async (req, res) => {
   const author = await db.getAuthorById(Number(authorId));
 
   if (!author) {
-    res.status(404).send("Author not found");
-    return;
+    throw new CustomNotFoundError("Author not found");
   }
 
   res.send(`Author Name: ${author.name}`);
